b69ba8b212da7599f083c6abe3e1065a5da11d01
ArtManis <Nyan@Moe.jap>
2018-04-12 23:49:01 +0300

    3 commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ robot.login("NDM0MDI2MDg2NjE2MDA2NjY2.DbEbAg.vRHTA7G7QR-_fo9Q1LBi0ymSXy4");
 
 robot.on('message',(message)=>{
  if(message.content=="торт"){
-message.reply("Да,я люблю торты!!");
+message.reply("Да,я люблю торты!!!");
  }
 }
 );
