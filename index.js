76c6059a9c581cedb07a73884fbce426305b1dd4
ArtManis <Nyan@Moe.jap>
2018-04-12 23:36:15 +0300

    add first

---
+++ index.js
@@ -0,0 +1,11 @@
+const Discord=require('discord.js');
+const robot=new Discord.Client();
+
+robot.login("NDM0MDI2MDg2NjE2MDA2NjY2.DbEbAg.vRHTA7G7QR-_fo9Q1LBi0ymSXy4");
+
+robot.on('message',(message)=>{
+ if(message.content=="торт"){
+message.reply("Да,я люблю торты!");
+ }
+}
+);
